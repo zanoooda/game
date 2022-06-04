4e9c9dfce5b2136085ce13733936a519d1ef3957
zanooda <id.nikto@gmail.com>
2022-06-04 19:09:43 +0300

    ...

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,25 @@
 const server = "http://localhost:3000";
 var socket;
-socket = io.connect(server);
-socket.on("connect", () => {
-  console.log("connect");
+
+const start = () => {
+  console.log("start");
+  socket = io.connect(server);
+  socket.on("connect", () => {
+    console.log("connect");
+  });
+
+  socket.on("counter", (n) => {
+    // online-users-counter
+    console.log(spinner, `${n - 1} users online.`);
+  });
+  socket.on("partner-found", (player) => {
+    console.log(`partner-found ${player}`);
+  });
+  socket.on('move', (move) => {
+    // validation
+    console.log(move);
 });
+};
+const move = () => {
+  socket.emit('move', {"data": "data"});
+}
--- server/index.js
+++ server/index.js
@@ -7,4 +7,51 @@ var io = require('socket.io')(server);
 
 io.on('connection', function (socket) {
     console.log(socket.id, 'connected');
+
+    socket.partnerId = getPartnerId(socket, io);
+    if(socket.partnerId) {
+        console.log(socket.id, 'partner-found');
+
+        io.sockets.connected[socket.partnerId].partnerId = socket.id;
+
+        io.sockets.connected[socket.partnerId].emit('partner-found', 0);
+        socket.emit('partner-found', 1);
+    }
+
+    socket.on('move', function(move) {
+        console.log(`${socket.id}, move ${move}`);
+        // validation
+        if(socket.partnerId && io.sockets.connected[socket.partnerId]) {
+            socket.broadcast.to(socket.partnerId).emit('move', move);
+        }
+    });
+
+    socket.on('disconnect', function() {
+        console.log(socket.id, 'disconnect');
+
+        if(socket.partnerId && io.sockets.connected[socket.partnerId]) {
+            io.sockets.connected[socket.partnerId].disconnect();
+        }
+    });
 });
+
+function getPartnerId(socket, io) {
+    var partnerId = false;
+
+    var b = false;
+    var ids = Object.keys(io.sockets.connected);
+    ids.forEach(function(id) {
+        var s = io.sockets.connected[id];
+        if(
+            !b && 
+            s.id != socket.id &&
+            s.partnerId !== undefined && 
+            !s.partnerId
+        ) {
+            partnerId = s.id;
+            b = true;
+        }
+    });
+
+    return partnerId;
+}
